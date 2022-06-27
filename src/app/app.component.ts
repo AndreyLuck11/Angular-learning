09b13e1508f3a863d0f4a50fee8488a6110a61f5
Andrew <andrew_shevchuk21@protonmail.com>
2022-06-28 00:50:43 +0300

    base http services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Todo, TodoService} from './servises/todo.service';
 
-export interface Todo {
-  completed: boolean;
-  title: string;
-  id?: number;
-}
+
 
 @Component({
   selector: 'app-root',
@@ -16,7 +12,7 @@ export class AppComponent implements OnInit {
   todos: Todo[] = [];
   todoTitle = '';
   loading = false;
-  constructor(private http: HttpClient) {}
+  constructor(private todosService: TodoService) {}
   ngOnInit() {
     this.fetchTodo();
   }
@@ -25,27 +21,25 @@ export class AppComponent implements OnInit {
     if (!this.todoTitle.trim()) {
       return;
     }
-    const newTodo: Todo = {
-        title: this.todoTitle,
-        completed: false
-      };
-    this.http.post('https://jsonplaceholder.typicode.com/todos', newTodo).subscribe((todo) => {
-      console.log(todo);
-      this.todos.push(todo as Todo);
+    this.todosService.addTodo({
+      title: this.todoTitle,
+      completed: false
+    }).subscribe(todo => {
+      this.todos.push(todo);
       this.todoTitle = '';
     });
   }
 
   fetchTodo() {
     this.loading = true;
-    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2').subscribe(response => {
-      this.todos = response;
+    this.todosService.fetchTodos().subscribe(todos => {
+      this.todos = todos;
       this.loading = false;
     });
   }
 
   removeTodo(id: number) {
-    this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        this.todosService.removeTodo(id)
         .subscribe(() => {
           // this.todos = this.todos.filter(t => t.id !== id);
           console.log(this.todos);
@@ -55,6 +49,12 @@ export class AppComponent implements OnInit {
               console.log(this.todos);
             }
           });
+    });
+  }
+
+  complitedTodo(id: number) {
+    this.todosService.complitedTodo(id).subscribe(todo => {
+      this.todos.find(t => t.id === todo.id).completed = true;
     });
   }
 }
