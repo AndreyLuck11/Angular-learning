c6cee03f0c95db1727c1627811ee9a5d160d76d1
Andrew <andrew_shevchuk21@protonmail.com>
2022-06-29 01:26:19 +0300

    errors and child routing

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Post, PostsService} from '../posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements  OnInit {
+  post: Post;
+  constructor(private rout: ActivatedRoute,
+              private router: Router,
+              private postService: PostsService) {
+  }
 
+  ngOnInit() {
+    this.rout.params.subscribe((params: Params) => {
+      console.log('Params', params);
+      this.post = this.postService.getById(+params.id);
+    });
+  }
+
+  loadPost() {
+    this.router.navigate(['/posts', 44]);
+  }
 }
